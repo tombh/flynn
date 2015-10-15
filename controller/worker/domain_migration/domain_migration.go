bbb70671e92037492c232938830237396da4e29f
Jesse Stuart <jesse@jessestuart.ca>
2015-10-14 19:30:44 -0500

    controller WIP: refactor
    Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

--- controller/worker/domain_migration/domain_migration.go
+++ controller/worker/domain_migration/domain_migration.go
@@ -22,6 +22,13 @@ type context struct {
 	logger log15.Logger
 }
 
+type migration struct {
+	db     *postgres.DB
+	client *controller.Client
+	logger log15.Logger
+	dm     *ct.DomainMigration
+}
+
 func JobHandler(db *postgres.DB, client *controller.Client, logger log15.Logger) func(*que.Job) error {
 	return (&context{db, client, logger}).HandleDomainMigration
 }
@@ -38,9 +45,16 @@ func (c *context) HandleDomainMigration(job *que.Job) (err error) {
 
 	log = log.New("domain_migration", dm.ID)
 
-	if err := c.db.QueryRow("SELECT old_domain, domain, old_tls_cert, tls_cert, created_at, finished_at FROM domain_migrations WHERE migration_id = $1", dm.ID).Scan(&dm.OldDomain, &dm.Domain, &dm.OldTLSCert, &dm.TLSCert, &dm.CreatedAt, &dm.FinishedAt); err != nil {
+	m := &migration{
+		db:     c.db,
+		client: c.client,
+		logger: log,
+		dm:     dm,
+	}
+
+	if err := m.db.QueryRow("SELECT old_domain, domain, old_tls_cert, tls_cert, created_at, finished_at FROM domain_migrations WHERE migration_id = $1", dm.ID).Scan(&dm.OldDomain, &dm.Domain, &dm.OldTLSCert, &dm.TLSCert, &dm.CreatedAt, &dm.FinishedAt); err != nil {
 		log.Error("error fetching postgres record", "err", err)
-		c.createEvent(dm, err)
+		m.createEvent(err)
 		return err
 	}
 
@@ -49,68 +63,73 @@ func (c *context) HandleDomainMigration(job *que.Job) (err error) {
 		return nil
 	}
 
+	return m.Run()
+}
+
+func (m *migration) Run() error {
+	log := m.logger
+	dm := m.dm
+
 	// Generate TLS Cert if not already present
 	var cert *tlscert.Cert
 	if dm.TLSCert == nil {
 		var err error
-		cert, err = c.generateTLSCert(dm.ID, dm.Domain)
+		cert, err = m.generateTLSCert()
 		if err != nil {
 			log.Error("error generating TLS cert", "err", err)
-			c.createEvent(dm, err)
+			m.createEvent(err)
 			return err
 		}
-	} else {
-		cert = dm.TLSCert
+		dm.TLSCert = cert
 	}
-	oldCert := dm.OldTLSCert
 
-	if err := c.maybeDeployController(oldCert, cert, dm.OldDomain, dm.Domain); err != nil {
+	if err := m.maybeDeployController(); err != nil {
 		log.Error("error deploying controller", "err", err)
-		c.createEvent(dm, err)
+		m.createEvent(err)
 		return err
 	}
 
-	if err := c.maybeDeployRouter(oldCert, cert, dm.OldDomain, dm.Domain); err != nil {
+	if err := m.maybeDeployRouter(); err != nil {
 		log.Error("error deploying router", "err", err)
-		c.createEvent(dm, err)
+		m.createEvent(err)
 		return err
 	}
 
-	if err := c.maybeDeployDashboard(oldCert, cert, dm.OldDomain, dm.Domain); err != nil {
+	if err := m.maybeDeployDashboard(); err != nil {
 		log.Error("error deploying dashboard", "err", err)
-		c.createEvent(dm, err)
+		m.createEvent(err)
 		return err
 	}
 
-	if err := c.createMissingRoutes(oldCert, cert, dm.OldDomain, dm.Domain); err != nil {
+	if err := m.createMissingRoutes(); err != nil {
 		log.Error("error creating missing routes", "err", err)
-		c.createEvent(dm, err)
+		m.createEvent(err)
 		return err
 	}
 
-	if err := c.db.QueryRow("UPDATE domain_migrations SET finished_at = now() RETURNING finished_at").Scan(&dm.FinishedAt); err != nil {
+	if err := m.db.QueryRow("UPDATE domain_migrations SET finished_at = now() RETURNING finished_at").Scan(&dm.FinishedAt); err != nil {
 		log.Error("error setting finished_at", "err", err)
-		c.createEvent(dm, err)
+		m.createEvent(err)
 		return err
 	}
 
 	log.Info("domain migration complete")
 
-	c.createEvent(dm, nil)
+	m.createEvent(nil)
 
 	return nil
 }
 
-func (c *context) generateTLSCert(id, domain string) (*tlscert.Cert, error) {
+func (m *migration) generateTLSCert() (*tlscert.Cert, error) {
 	hosts := []string{
-		domain,
-		fmt.Sprintf("*.%s", domain),
+		m.dm.Domain,
+		fmt.Sprintf("*.%s", m.dm.Domain),
 	}
 	cert, err := tlscert.Generate(hosts)
 	if err != nil {
 		return nil, err
 	}
-	if _, err := c.db.Query("UPDATE domain_migrations SET tls_cert = $1 WHERE migration_id = $2", cert, id); err != nil {
+	if _, err := m.db.Query("UPDATE domain_migrations SET tls_cert = $1 WHERE migration_id = $2", cert, m.dm.ID); err != nil {
 		return nil, err
 	}
 	return cert, nil
@@ -125,8 +144,8 @@ func dupRelease(release *ct.Release) *ct.Release {
 	}
 }
 
-func (c *context) waitForDeployment(app string) error {
-	list, err := c.client.DeploymentList(app)
+func (m *migration) waitForDeployment(app string) error {
+	list, err := m.client.DeploymentList(app)
 	if err != nil || len(list) == 0 {
 		return err
 	}
@@ -136,7 +155,7 @@ func (c *context) waitForDeployment(app string) error {
 	}
 
 	events := make(chan *ct.Event)
-	stream, err := c.client.StreamEvents(controller.StreamEventsOptions{
+	stream, err := m.client.StreamEvents(controller.StreamEventsOptions{
 		AppID:       d.AppID,
 		ObjectID:    d.ID,
 		ObjectTypes: []ct.EventType{ct.EventTypeDeployment},
@@ -166,79 +185,79 @@ func (c *context) waitForDeployment(app string) error {
 	}
 }
 
-func (c *context) maybeDeployController(oldCert, cert *tlscert.Cert, oldDomain, newDomain string) error {
+func (m *migration) maybeDeployController() error {
 	const appName = "controller"
-	if err := c.waitForDeployment(appName); err != nil {
+	if err := m.waitForDeployment(appName); err != nil {
 		return err
 	}
-	release, err := c.client.GetAppRelease(appName)
+	release, err := m.client.GetAppRelease(appName)
 	if err != nil {
 		return err
 	}
-	if release.Env["DEFAULT_ROUTE_DOMAIN"] != oldDomain {
+	if release.Env["DEFAULT_ROUTE_DOMAIN"] != m.dm.OldDomain {
 		return nil
 	}
 	release = dupRelease(release)
-	release.Env["DEFAULT_ROUTE_DOMAIN"] = newDomain
-	release.Env["CA_CERT"] = cert.CACert
-	if err := c.client.CreateRelease(release); err != nil {
+	release.Env["DEFAULT_ROUTE_DOMAIN"] = m.dm.Domain
+	release.Env["CA_CERT"] = m.dm.TLSCert.CACert
+	if err := m.client.CreateRelease(release); err != nil {
 		return err
 	}
-	return c.client.DeployAppRelease(appName, release.ID)
+	return m.client.DeployAppRelease(appName, release.ID)
 }
 
-func (c *context) maybeDeployRouter(oldCert, cert *tlscert.Cert, oldDomain, newDomain string) error {
+func (m *migration) maybeDeployRouter() error {
 	const appName = "router"
-	if err := c.waitForDeployment(appName); err != nil {
+	if err := m.waitForDeployment(appName); err != nil {
 		return err
 	}
-	release, err := c.client.GetAppRelease(appName)
+	release, err := m.client.GetAppRelease(appName)
 	if err != nil {
 		return err
 	}
-	if release.Env["TLSCERT"] != oldCert.Cert {
+	if release.Env["TLSCERT"] != m.dm.OldTLSCert.Cert {
 		return nil
 	}
 	release = dupRelease(release)
-	release.Env["TLSCERT"] = cert.Cert
-	release.Env["TLSKEY"] = cert.PrivateKey
-	if err := c.client.CreateRelease(release); err != nil {
+	release.Env["TLSCERT"] = m.dm.TLSCert.Cert
+	release.Env["TLSKEY"] = m.dm.TLSCert.PrivateKey
+	if err := m.client.CreateRelease(release); err != nil {
 		return err
 	}
-	return c.client.DeployAppRelease(appName, release.ID)
+	return m.client.DeployAppRelease(appName, release.ID)
 }
 
-func (c *context) maybeDeployDashboard(oldCert, cert *tlscert.Cert, oldDomain, newDomain string) error {
+func (m *migration) maybeDeployDashboard() error {
 	const appName = "dashboard"
-	if err := c.waitForDeployment(appName); err != nil {
+	if err := m.waitForDeployment(appName); err != nil {
 		return err
 	}
-	release, err := c.client.GetAppRelease(appName)
+	release, err := m.client.GetAppRelease(appName)
 	if err != nil {
 		return err
 	}
-	if release.Env["DEFAULT_ROUTE_DOMAIN"] != oldDomain {
+	if release.Env["DEFAULT_ROUTE_DOMAIN"] != m.dm.OldDomain {
 		return nil
 	}
 	release = dupRelease(release)
-	release.Env["CA_CERT"] = cert.CACert
-	release.Env["DEFAULT_ROUTE_DOMAIN"] = newDomain
-	release.Env["CONTROLLER_DOMAIN"] = fmt.Sprintf("controller.%s", newDomain)
-	release.Env["URL"] = fmt.Sprintf("dashboard.%s", newDomain)
-	if err := c.client.CreateRelease(release); err != nil {
+	release.Env["CA_CERT"] = m.dm.TLSCert.CACert
+	release.Env["DEFAULT_ROUTE_DOMAIN"] = m.dm.Domain
+	release.Env["CONTROLLER_DOMAIN"] = fmt.Sprintf("controller.%s", m.dm.Domain)
+	release.Env["URL"] = fmt.Sprintf("dashboard.%s", m.dm.Domain)
+	if err := m.client.CreateRelease(release); err != nil {
 		return err
 	}
-	return c.client.DeployAppRelease(appName, release.ID)
+	return m.client.DeployAppRelease(appName, release.ID)
 }
 
-func (c *context) createMissingRoutes(oldCert, cert *tlscert.Cert, oldDomain, newDomain string) error {
-	apps, err := c.client.AppList()
+func (m *migration) createMissingRoutes() error {
+	apps, err := m.client.AppList()
 	if err != nil {
 		return err
 	}
 	errChan := make(chan error, len(apps))
 	createMissingRoutes := func(app *ct.App) {
-		errChan <- c.appCreateMissingRoutes(app.ID, oldCert, cert, oldDomain, newDomain)
+		errChan <- m.appCreateMissingRoutes(app.ID)
 	}
 	for _, app := range apps {
 		go createMissingRoutes(app)
@@ -252,8 +271,8 @@ func (c *context) createMissingRoutes(oldCert, cert *tlscert.Cert, oldDomain, ne
 	return nil
 }
 
-func (c *context) appCreateMissingRoutes(appID string, oldCert, cert *tlscert.Cert, oldDomain, newDomain string) error {
-	routes, err := c.client.RouteList(appID)
+func (m *migration) appCreateMissingRoutes(appID string) error {
+	routes, err := m.client.RouteList(appID)
 	if err != nil {
 		return err
 	}
@@ -261,8 +280,8 @@ func (c *context) appCreateMissingRoutes(appID string, oldCert, cert *tlscert.Ce
 		if route.Type != "http" {
 			continue
 		}
-		if strings.HasSuffix(route.Domain, oldDomain) && route.TLSCert == oldCert.Cert {
-			if err := c.appMaybeCreateRoute(appID, oldCert, cert, route, strings.TrimSuffix(route.Domain, oldDomain), newDomain, routes); err != nil {
+		if strings.HasSuffix(route.Domain, m.dm.OldDomain) && route.TLSCert == m.dm.OldTLSCert.Cert {
+			if err := m.appMaybeCreateRoute(appID, route, routes); err != nil {
 				return err
 			}
 		}
@@ -270,38 +289,39 @@ func (c *context) appCreateMissingRoutes(appID string, oldCert, cert *tlscert.Ce
 	return nil
 }
 
-func (c *context) appMaybeCreateRoute(appID string, oldCert, cert *tlscert.Cert, oldRoute *router.Route, prefix, domain string, routes []*router.Route) error {
+func (m *migration) appMaybeCreateRoute(appID string, oldRoute *router.Route, routes []*router.Route) error {
+	prefix := strings.TrimSuffix(oldRoute.Domain, m.dm.OldDomain)
 	for _, route := range routes {
-		if strings.HasPrefix(route.Domain, prefix) && strings.HasSuffix(route.Domain, domain) {
+		if strings.HasPrefix(route.Domain, prefix) && strings.HasSuffix(route.Domain, m.dm.Domain) {
 			// Route already exists
 			return nil
 		}
 	}
 	route := &router.Route{
 		Type:    "http",
-		Domain:  strings.Join([]string{prefix, domain}, ""),
+		Domain:  strings.Join([]string{prefix, m.dm.Domain}, ""),
 		Sticky:  oldRoute.Sticky,
 		Service: oldRoute.Service,
 	}
-	if oldRoute.TLSCert == oldCert.Cert {
-		route.TLSCert = cert.Cert
-		route.TLSKey = cert.PrivateKey
+	if oldRoute.TLSCert == m.dm.OldTLSCert.Cert {
+		route.TLSCert = m.dm.TLSCert.Cert
+		route.TLSKey = m.dm.TLSCert.PrivateKey
 	} else {
 		route.TLSCert = oldRoute.TLSCert
 		route.TLSKey = oldRoute.TLSKey
 	}
-	err := c.client.CreateRoute(appID, route)
+	err := m.client.CreateRoute(appID, route)
 	if err != nil && err.Error() == "conflict: Duplicate route" {
 		return nil
 	}
 	return err
 }
 
-func (c *context) createEvent(dm *ct.DomainMigration, err error) error {
-	e := ct.DomainMigrationEvent{DomainMigration: dm}
+func (m *migration) createEvent(err error) error {
+	e := ct.DomainMigrationEvent{DomainMigration: m.dm}
 	if err != nil {
 		e.Error = err.Error()
 	}
 	query := "INSERT INTO events (object_id, object_type, data) VALUES ($1, $2, $3)"
-	return c.db.Exec(query, dm.ID, string(ct.EventTypeDomainMigration), e)
+	return m.db.Exec(query, m.dm.ID, string(ct.EventTypeDomainMigration), e)
 }
